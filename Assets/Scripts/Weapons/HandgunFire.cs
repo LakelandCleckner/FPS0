1a0b78bd8752244fa95ef595bdffca2fdd9c032d
LakelandCleckner <lakeland.cleckner@hotmail.com>
2026-02-19 21:42:21 -0500

    Crosshairs and added crosshair animation on shooting handgun.

--- Assets/Scripts/Weapons/HandgunFire.cs
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -7,6 +7,7 @@ public class HandgunFire : MonoBehaviour
 
     [SerializeField] AudioSource gunFire;
     [SerializeField] GameObject handgun;
+    [SerializeField] GameObject crosshair;
     [SerializeField] bool canFire = true;
     void Update()
     {
@@ -24,8 +25,10 @@ public class HandgunFire : MonoBehaviour
     {
         gunFire.Play();
         handgun.GetComponent<Animator>().Play("HandgunFire");
+        crosshair.GetComponent<Animator>().Play("HandgunFireCrosshair");
         yield return new WaitForSeconds(0.42857f);
         handgun.GetComponent<Animator>().Play("New State");
+        crosshair.GetComponent<Animator>().Play("New State");
         canFire = true;
     }
 }
