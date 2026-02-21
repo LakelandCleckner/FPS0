3b6d65257d18b4140a9837f31be288b71b2d4d8f
LakelandCleckner <lakeland.cleckner@hotmail.com>
2026-02-21 02:12:50 -0500

    Ammo Counter, Ammo Pickup, Player Raycast

--- Assets/Scripts/Weapons/HandgunFire.cs
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -9,14 +9,24 @@ public class HandgunFire : MonoBehaviour
     [SerializeField] GameObject handgun;
     [SerializeField] GameObject crosshair;
     [SerializeField] bool canFire = true;
+    [SerializeField] AudioSource emptyGunSound;
     void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             if (canFire == true)
             {
-                canFire = false;
-                StartCoroutine(FiringGun());
+                if (GlobalAmmo.handgunAmmoCount == 0)
+                {
+                    canFire = false;
+                    StartCoroutine(EmptyGun());
+                }
+                else
+                {
+                    canFire = false;
+                    StartCoroutine(FiringGun());
+                }
+                
             }
         }
     }
@@ -24,11 +34,19 @@ public class HandgunFire : MonoBehaviour
     IEnumerator FiringGun()
     {
         gunFire.Play();
+        GlobalAmmo.handgunAmmoCount -= 1;
         handgun.GetComponent<Animator>().Play("HandgunFire");
         crosshair.GetComponent<Animator>().Play("HandgunFireCrosshair");
         yield return new WaitForSeconds(0.42857f);
         handgun.GetComponent<Animator>().Play("New State");
         crosshair.GetComponent<Animator>().Play("New State");
+        canFire = true;
+    }
+
+    IEnumerator EmptyGun()
+    {
+        emptyGunSound.Play();
+        yield return new WaitForSeconds(0.6f);
         canFire = true;
     }
 }
